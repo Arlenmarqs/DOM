cb72b83632f07e6a92e15e6eed711b496bfcb8c8
Arlen marques <Arlenmarqs@gmail.com>
2025-09-17 22:09:06 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+
+// let titulo2 = document.querySelector(".titulo2")
+// let titulo3 = document.querySelector("#titulo3")
+// let titulo = document.querySelector("h1")
+
+// // Escrevendo dentro de tags
+// titulo.innerText = "Olá mundo"
+// titulo2.innerText = "Olá mundo"
+// titulo3.innerText = "Olá mundo"
+
+// let div = document.querySelector("div")
+// div.innerHTML = "<h1> buuuufo </h1>"
+
+// titulo.classList.add("title1")
+// titulo2.classList.add("title2")
+// titulo3.classList.add("title3")
+
+// // remove
+// titulo.classList.remove("title1")
+// titulo2.classList.add("title2")
+// titulo3.classList.add("title3")
+
+// let button = document.querySelector("button")
+
+// function renderTitle() {    
+//     let h1 = document.querySelector("h1")
+//     h1.innerText = "vai tomando" 
+// }
+
+// button.addEventListener("click", renderTitle)
+
+let icon = document.querySelector("icon")
+function darkmode() {  
+    let body = document.querySelector("body")
+    let = sectionIcon = document.querySelector("teste")
+    let = icon = document.querySelector("icon")
+    body.classList.toggle("body-darkmode")
+    sectionIcon.classList.toggle("section-darkmode")
+    icon.classList.toggle("icon-class")
+}
+icon.addEventListener("click",darkmode)
